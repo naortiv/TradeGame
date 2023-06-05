f988839a4507c419294c2985acf53a66c7808fd5
NaorTiv <naortiv@gmail.com>
2023-06-05 21:34:10 +0300

    api for stock price update

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 import random
+import requests
+
+ALPHA_VANTAGE_API_KEY = 'YOUR_API_KEY'
+
+def get_stock_price(ticker):
+    url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={ALPHA_VANTAGE_API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    if 'Global Quote' in data:
+        return float(data['Global Quote']['05. price'])
+    else:
+        return None
 
 def play_game():
-    print("Welcome to the Guessing Game!")
-    print("I have chosen a number between 1 and 100.")
-    print("Can you guess the number?")
-    print("--------------------------------------")
+    print("Welcome to the Stock Price Guessing Game!")
+    print("-----------------------------------------")
+    print("Choose a stock by its ticker symbol and guess if the stock price will go up or down.")
+    print("I will give you the current stock price, and you have to make your prediction.")
+    print("-----------------------------------------")
     
-    secret_number = random.randint(1, 100)
-    attempts = 0
-    max_attempts = 10
+    ticker = input("Enter the stock ticker symbol (e.g., AAPL, GOOGL, TSLA): ")
+    stock_price = get_stock_price(ticker)
     
-    while attempts < max_attempts:
-        guess = int(input("Enter your guess: "))
-        
-        if guess < secret_number:
-            print("Too low!")
-        elif guess > secret_number:
-            print("Too high!")
-        else:
-            print("Congratulations! You guessed the number correctly!")
-            break
-        
-        attempts += 1
-        print("Attempts remaining:", max_attempts - attempts)
-        print("--------------------------------------")
+    if stock_price is None:
+        print("Error: Failed to fetch the stock price. Please try again.")
+        return
     
-    if attempts == max_attempts:
-        print("Game over! You couldn't guess the number. The secret number was:", secret_number)
+    print(f"The current price of {ticker} is ${stock_price:.2f}")
+    
+    direction = random.choice(["up", "down"])
+    guess = input("Will the stock price go up or down? Enter 'up' or 'down': ")
+    
+    if guess.lower() == direction:
+        print("Congratulations! You guessed correctly!")
+    else:
+        print("Oops! You guessed incorrectly.")
+    
+    print(f"The stock price was predicted to go {direction}.")
+    print("-----------------------------------------")
 
 play_game()
